db80c319b64fe69d4cef94c526de54e5b69dc5c8
heterocomia <116381993+heterocomia@users.noreply.github.com>
2022-10-21 16:01:20 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -10,7 +10,7 @@ st.title("monobolistica!")
 st.header("ovos em tudo!")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Bem vindos!")
+st.subheader("Bem vindos ao mononucleose!")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
